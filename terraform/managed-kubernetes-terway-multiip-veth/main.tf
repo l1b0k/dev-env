3a9062e905641110308688f17146f44bfd8330bc
l1b0k <libokang.dev@gmail.com>
2021-01-13 11:17:29 +0800

    add alicloud terraform

---
+++ terraform/managed-kubernetes-terway-multiip-veth/main.tf
@@ -0,0 +1,50 @@
+// If there is not specifying vpc_id, the module will launch a new vpc
+resource "alicloud_vpc" "vpc" {
+  name       = "terway-mutlip-veth"
+  count      = var.vpc_id == "" ? 1 : 0
+  cidr_block = var.vpc_cidr
+}
+
+// According to the vswitch cidr blocks to launch several vswitches
+resource "alicloud_vswitch" "vswitches" {
+  count             = length(var.vswitch_ids) > 0 ? 0 : length(var.vswitch_cidrs)
+  vpc_id            = var.vpc_id == "" ? join("", alicloud_vpc.vpc.*.id) : var.vpc_id
+  cidr_block        = element(var.vswitch_cidrs, count.index)
+  availability_zone = element(var.availability_zone, count.index)
+}
+
+
+// According to the vswitch cidr blocks to launch several vswitches
+resource "alicloud_vswitch" "terway_vswitches" {
+  count             = length(var.terway_vswitch_ids) > 0 ? 0 : length(var.terway_vswitch_cirds)
+  vpc_id            = var.vpc_id == "" ? join("", alicloud_vpc.vpc.*.id) : var.vpc_id
+  cidr_block        = element(var.terway_vswitch_cirds, count.index)
+  availability_zone = element(var.availability_zone, count.index)
+}
+
+resource "alicloud_cs_managed_kubernetes" "k8s" {
+  name                  = "terway-multiip-veth"
+  count                 = var.k8s_number
+  worker_vswitch_ids    = length(var.vswitch_ids) > 0 ? split(",", join(",", var.vswitch_ids)): length(var.vswitch_cidrs) < 1 ? [] : split(",", join(",", alicloud_vswitch.vswitches.*.id))
+  pod_vswitch_ids       = length(var.terway_vswitch_ids) > 0 ? split(",", join(",", var.terway_vswitch_ids)): length(var.terway_vswitch_cirds) < 1 ? [] : split(",", join(",", alicloud_vswitch.terway_vswitches.*.id))
+  worker_instance_types = var.worker_instance_types
+  worker_number         = var.worker_number
+  node_cidr_mask        = var.node_cidr_mask
+  enable_ssh            = var.enable_ssh
+  install_cloud_monitor = var.install_cloud_monitor
+  cpu_policy            = var.cpu_policy
+  proxy_mode            = var.proxy_mode
+  image_id              = "aliyun_2_1903_x64_20G_alibase_20200904.vhd"
+  key_name              = var.key_name
+  service_cidr          = var.service_cidr
+  version               = "1.18.8-aliyun.1"
+  cluster_spec          = "ack.pro.small" // ack.standard | ack.pro.small
+  dynamic "addons" {
+      for_each = var.cluster_addons
+      content {
+        name                    = lookup(addons.value, "name", var.cluster_addons)
+        config                  = lookup(addons.value, "config", var.cluster_addons)
+      }
+  }
+}
+
